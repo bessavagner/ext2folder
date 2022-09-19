d06593dcffe66ac5ff99b8ddcfcae87fd0786174
Vagner Bessa <bessavagner@gmail.com>
2022-09-19 15:01:58 -0300

    add single script. No prompt

---
+++ ext2folders.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+import pathlib
+
+folder = {
+    "jpg": "images",
+    "png": "images",
+    "ico": "images",
+    "gif": "images",
+    "svg": "images",
+    "sql": "sql",
+    "deb": "programs",
+    "exe": "programs",
+    "msi": "programs",
+    "pdf": "texts",
+    "xlsx": "excel",
+    "csv": "excel",
+    "rar": "archive",
+    "zip": "archive",
+    "gz": "archive",
+    "tar": "archive",
+    "docx": "texts",
+    "torrent": "torrent",
+    "txt": "texts",
+    "ipynb": "python",
+    "py": "python",
+    "pptx": "powerpoint",
+    "ppt": "powerpoint",
+    "mp3": "audio",
+    "wav": "audio",
+    "mp4": "video",
+    "m3u8": "video",
+    "webm": "video",
+    "ts": "video",
+    "json": "json",
+    "css": "web",
+    "js": "web",
+    "html": "web",
+    "apk": "apk",
+    "sqlite3": "sqlite3",
+}
+
+# You can run this from any location, except target folder
+LOCATION = str(pathlib.Path.home() / "Downloads")
+files = []
+for (dirpath, dirnames, filenames) in os.walk(LOCATION):
+    files.extend(filenames)
+    break  # Walk throught only LOCATION files, not subfiles
+for file in files:
+    ext = file.split(".")[-1]  # extracts extensions
+    ext_data = folder.keys()
+    # check if it has a category. If not, receives 'no_category'
+    folder_name = folder[ext] if ext in ext_data else "no_category"
+    path = os.path.join(LOCATION, folder_name)
+    if not os.path.exists(path):  # creates folder if it doesn't exist
+        os.mkdir(path)
+    shutil.move(os.path.join(LOCATION,file), path)  # the move
